4939187eddcada6847fd474709baf71e412a75f2
SandeeepS <sandeepsanthosh3303@gmail.com>
2024-07-22 22:34:48 +0530

    created cahnges on Chat component

--- src/Components/ChatComponent/Chat.js
+++ src/Components/ChatComponent/Chat.js
@@ -1,34 +1,74 @@
-import React from 'react'
+import React from "react";
 import "./Chat.css";
-import { Avatar, IconButton } from '@mui/material';
-import { AttachFile, MoreVert, SearchOutlined } from '@mui/icons-material';
+import { Avatar, IconButton } from "@mui/material";
+import {
+  AttachFile,
+  InsertEmoticon,
+  Mic,
+  MoreVert,
+  SearchOutlined,
+} from "@mui/icons-material";
 
 function Chat() {
   return (
-    <div className='chat'>
-        <div className="chat__header">
-            <Avatar/>
+    <div className="chat">
+      <div className="chat__header">
+        <Avatar />
 
-            <div className="chat__headerInfo">
-                <h3>Room name</h3>
-                <p>Last seen at ....</p>
-            </div>
-
-            <div className="chat__headerRight">
-                <IconButton>
-                    <SearchOutlined/>
-                </IconButton>
-                <IconButton>
-                    <AttachFile/>
-                </IconButton>
-                <IconButton>
-                    <MoreVert/>
-                </IconButton>
-            </div>
-            
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at ....</p>
         </div>
+
+        <div className="chat__headerRight">
+          <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className="chat__body">
+        <p className="chat__message">
+          <span className="chat__name"> Sonny </span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className="chat__message">
+          <span className="chat__name"> Sonny </span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+
+        <p className=" chat__message chat__reciever">
+          <span className="chat__name"> Sonny </span>
+          This is a message
+          <span className="chat__timestamp">{new Date().toUTCString()}</span>
+        </p>
+      </div>
+
+      <div className="chat__footer">
+        <InsertEmoticon />
+        <form action="">
+          <input
+            placeholder="Type a message"
+            type="text"
+          />
+
+          <button type="submit">
+            Send a message
+          </button>
+        </form>
+        <Mic/>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Chat
+export default Chat;
